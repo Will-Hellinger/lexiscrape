e2f66a1cc8477f895bdc7b0dd6bf4ad605a81d52
Will-Hellinger <will.hellinger@gmail.com>
2024-05-20 06:01:13 -0500

    definition cleaner created

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
+import json
 import time
 import string
+import hashlib
 import requests
 from bs4 import BeautifulSoup
+
+
+def definition_cleaner(definition: str) -> str:
+    if '[' in definition and ']' in definition:
+        start = definition.index('[')
+        end = definition.index(']') + 2
+
+        if end >= len(definition):
+            definition = definition[0:start]
+        else:
+            definition = definition[end:]
+    
+    if '(' in definition:
+        start = definition.index('(')
+
+        end = len(definition)
+        if ')' in definition:
+            end = definition.index(')') + 2
+
+        if end >= len(definition):
+            definition = definition[0:start]
+        else:
+            definition = definition[end:]
+
+    definitions = definition.split(', ')
+
+    while '-' in definitions:
+        definitions.remove('-')
+    
+    while '' in definitions:
+        definitions.remove('')
+
+    while ', ' in definitions:
+        definitions.remove(', ')
+
+    return definitions
 
 
 def get_word_info(url: str) -> dict:
@@ -9,8 +47,23 @@ def get_word_info(url: str) -> dict:
     soup = BeautifulSoup(response.text, 'html.parser')
 
     flash_card_title = soup.find('div', {'class': 'flash_card_title'})
+    flash_card_english_def = soup.find('ol', {'class': 'flash_card_english_def'})
 
     cleaned_title = ' '.join(flash_card_title.text.split())
+    definitions: list = []
+
+    definition_list = flash_card_english_def.find_all('li')
+    for definition in definition_list:
+        cleaned_definition = ' '.join(definition.text.split())
+        cleaned_definition = definition_cleaner(cleaned_definition)
+        definitions += cleaned_definition
+
+    word_info = {
+        'title': cleaned_title,
+        'definitions' : definitions
+    }
+
+    print(cleaned_title, definitions)
 
     return None
 
